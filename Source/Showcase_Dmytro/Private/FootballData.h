cf9f03cfa062179600f326241e2f447d59ee09b1
Dmytro <lem.pasq0236@gmail.com>
2026-07-04 08:32:59 -0700

    Initial commit

---
+++ Source/Showcase_Dmytro/Private/FootballData.h
@@ -0,0 +1,50 @@
+﻿// Showcase_Dmytro 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FootballData.generated.h"
+
+// Enumeration for Teams
+UENUM(BlueprintType)
+enum class ETeams : uint8
+{        
+	None,
+	TeamBlue,    
+	TeamRed     
+};
+
+// Enumeration for Ball States
+UENUM(BlueprintType)
+enum class EBallStates : uint8
+{
+	Free,
+	GrabbedByPlayer,
+	GrabbedByGoalKeeper
+};
+
+
+// Enumeration for Match States Handled in GameMode
+UENUM(BlueprintType)
+enum class EMatchStates : uint8
+{
+	InLobby,
+	OnGoingMatch,
+	Celebrating,
+	Finished
+};
+
+
+// Structure for Scoreboard Data
+USTRUCT(BlueprintType)
+struct FScoreboardData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 TeamBlueScore = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 TeamRedScore = 0;
+
+};
---
+++ Source/Showcase_Dmytro/Showcase_Dmytro.cpp
@@ -0,0 +1,6 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Showcase_Dmytro.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Showcase_Dmytro, "Showcase_Dmytro" );
