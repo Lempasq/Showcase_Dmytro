d93ee069c391706c72780a17acbb2faeb17ee47f
Dmytro <lem.pasq0236@gmail.com>
2026-07-05 22:10:13 -0700

    Fixed build configs and UI
    Fixed configs to make a build using UE 5.7
    Minor improvements for Gameplay UI

--- Source/Showcase_Dmytro.Target.cs
+++ Source/Showcase_Dmytro.Target.cs
@@ -8,7 +8,7 @@ public class Showcase_DmytroTarget : TargetRules
 	public Showcase_DmytroTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "Showcase_Dmytro" } );
 	}
